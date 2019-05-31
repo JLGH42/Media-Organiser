456c4bde27f8dc97c1c5a5f225aae12d52284447
JLGH42 <jordanlyn.98@hotmail.com>
2019-05-31 16:20:51 +0100

    removed redundant code // change name of file on upload

--- routes/layout.js
+++ routes/layout.js
@@ -4,20 +4,16 @@ const logger = require('../controller/logger.js');
 const multer = require('multer');
 const os = require('os');
 const fs = require('fs');
-// const data = { User: 'jordan' };
 
 var storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, 'uploads/')
+        cb(null, 'views/public/uploads/')
     },
     filename: function(req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now())
     }
 })
 
-// var upload = multer({
-//     dest: 'views/public/uploads/'
-// })
 var upload = multer({
     storage: storage,
 })
@@ -26,16 +22,11 @@ router.get('/', (req, res) => {
     const data = {
         User: os.userInfo()
     };
-    // fs.writeFile('logger.json', JSON.stringify(data), (err) => {
-    //     if (err) throw err;
-    // })
-    // console.log(data);
     res.render('layout.hbs');
 });
 
 router.post('/upload', (req, res) => {
     const file = req.file;
-    // const $ = document.querySelector();
     console.log(file);
     const fileText = req.body.mediaUploadComments;
 
