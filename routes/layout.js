424c542337bf99879ab4b01c8e982ebd66df8d04
JLGH42 <jordanlyn.98@hotmail.com>
2019-05-31 16:14:16 +0100

    change text to file name being uploaded

--- routes/layout.js
+++ routes/layout.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const logger = require('../controller/logger.js');
+const multer = require('multer');
 const os = require('os');
 const fs = require('fs');
 // const data = { User: 'jordan' };
 
-console.log(typeof logger.logUser);
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+
+// var upload = multer({
+//     dest: 'views/public/uploads/'
+// })
+var upload = multer({
+    storage: storage,
+})
+
 router.get('/', (req, res) => {
     const data = {
         User: os.userInfo()
@@ -16,13 +32,30 @@ router.get('/', (req, res) => {
     // console.log(data);
     res.render('layout.hbs');
 });
+
 router.post('/upload', (req, res) => {
-    const file = req.file.mediaFile;
+    const file = req.file;
+    // const $ = document.querySelector();
+    console.log(file);
     const fileText = req.body.mediaUploadComments;
-    const input = document.querySelector('input.media-file-upload');
-    input.on('submit', (e) => {
-        console.log(`${file} : ${fileText}`);
-    })
+
+    var img = fs.readFileSync(`${file}`)
+    var encode_image = img.toString('base64');
+    var finalImg = {
+        contentType: req.file.mimetype,
+        image: new Buffer(encode_image, 'base64')
+    };
+    console.log(finalImg);
+
+    // var img = fs.readFileSync(`${file}`, (err, data) => {
+    //     if (err) throw err;
+    //     console.log(data);
+    //     console.log(img);
+    // });
+    // const input = document.querySelector('input.media-file-upload');
+    // input.on('submit', (e) => {
+    //     console.log(`${file} : ${fileText}`);
+    // })
 
     res.redirect('/upload');
 })
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 1998;
 
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 const path = require('path');
 const partialsPath = path.join(__dirname + '/views/partials');
 const viewsPath = path.join(__dirname + '/views/templates');
@@ -14,8 +15,13 @@ app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 app.use(express.static(publicPath));
 
+app.use(bodyParser.json()); // for parsing application/json
+var urlencodedParser = bodyParser.urlencoded({
+    extended: true,
+});
+
 //test route
 var testRoute = require('./routes/layout.js');
-app.use('/', testRoute);
+app.use('/', urlencodedParser, testRoute);
 
 app.listen(port, () => console.log(`Server Running on Port ${port}`));
